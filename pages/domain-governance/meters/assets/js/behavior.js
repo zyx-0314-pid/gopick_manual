420d03b51667db89f8d8bcb1fdab96ef9c157c0e
pid-zyx-0314 <ian@profilesasiapacific.com>
2026-05-18 13:21:52 +0800

    feat: added new page for governance aof meter

---
+++ pages/domain-governance/meters/assets/js/behavior.js
@@ -0,0 +1,18 @@
+(function () {
+    'use strict';
+
+    function setFooterYear() {
+        var yearSpan = document.getElementById('footerYearSpan');
+        if (yearSpan) yearSpan.textContent = new Date().getFullYear();
+    }
+
+    function initPage() {
+        setFooterYear();
+    }
+
+    if (document.readyState === 'loading') {
+        document.addEventListener('DOMContentLoaded', initPage);
+    } else {
+        initPage();
+    }
+})();
